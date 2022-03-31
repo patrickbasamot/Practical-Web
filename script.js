d933530be605e0baec807cd610e90d3bb0c8f176
Patrick Basamot <patrickbasamot@gmail.com>
2022-03-31 10:06:56 +0800

    Added another method to display data using forEach loop

--- script.js
+++ script.js
@@ -11,13 +11,43 @@ async function fetchData() {
         
 
 //Call the function fetchData    
-fetchData();
+fetchData().catch(error =>{
+    console.error(error);
+});
 
 
 function display(data){
 
+            //Using for loop
+    // for(let i=0; i<data.length; i++){
 
-    for(let i=0; i<data.length; i++){
+    //     //Create new element in every loop
+    //     const div = document.createElement('div')
+    //     const name = document.createElement('h3');
+    //     const description = document.createElement('p');
+    //     const img = document.createElement('img');
+    //     const productLink = document.createElement('a');
+ 
+    //     //Assign the data in every element that created above to be displayed in HTML
+    //     name.innerText =data[i].name;
+    //     description.innerText = data[i].description;
+    //     img.src = data[i].image_link;
+    //     img.alt = data[i].name;
+    //     productLink.href = data[i].product_link;
+    //     productLink.innerText = 'Product Page';
+
+    //      //Structure the layout 
+    //     container.appendChild(div);
+
+    //     div.appendChild(img);
+    //     div.appendChild(name);
+    //     div.appendChild(productLink);
+    //     div.appendChild(description);
+    
+    // }
+
+    //Using forEach loop
+    data.forEach(row => {
 
         //Create new element in every loop
         const div = document.createElement('div')
@@ -25,13 +55,14 @@ function display(data){
         const description = document.createElement('p');
         const img = document.createElement('img');
         const productLink = document.createElement('a');
- 
+    
+    
         //Assign the data in every element that created above to be displayed in HTML
-        name.innerText =data[i].name;
-        description.innerText = data[i].description;
-        img.src = data[i].image_link;
-        img.alt = data[i].name;
-        productLink.href = data[i].product_link;
+        name.innerText =row.name;
+        description.innerText = row.description;
+        img.src = row.image_link;
+        img.alt = row.name;
+        productLink.href = row.product_link;
         productLink.innerText = 'Product Page';
 
          //Structure the layout 
@@ -41,7 +72,7 @@ function display(data){
         div.appendChild(name);
         div.appendChild(productLink);
         div.appendChild(description);
-    
-    }
+
+    });
 }
 
