a7beef2d5072bfb9e6ae12ef5f575978862f37c9
Patrick Basamot <patrickbasamot@gmail.com>
2022-04-29 09:30:39 +0800

    delete some commented code

--- script.js
+++ script.js
@@ -29,35 +29,9 @@ fetchData().catch(error =>{
 
 
 function display(data){
-            //Using for loop
 
-    // for(let i=0; i<data.length; i++){
 
-    //     //Create new element in every loop
-    //     const div = document.createElement('div')
-    //     const name = document.createElement('h3');
-    //     const description = document.createElement('p');
-    //     const img = document.createElement('img');
-    //     const productLink = document.createElement('a');
- 
-    //     //Assign the data in every element that created above to be displayed in HTML
-
-    //     name.innerText =data[i].name;
-    //     description.innerText = data[i].description;
-    //     img.src = data[i].image_link;
-    //     img.alt = data[i].name;
-    //     productLink.href = data[i].product_link;
-    //     productLink.innerText = 'Product Page';
-
-    //      //Structure the layout 
-
-    //     container.appendChild(div);
-    //     div.appendChild(img);
-    //     div.appendChild(name);
-    //     div.appendChild(productLink);
-    //     div.appendChild(description);
-    
-    // }
+   
 
 
     //Using forEach loop
